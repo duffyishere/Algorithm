50e866b06d3dc14c5443914309edc36cb49f4a6d
zzunho9 <jamlove4763@gmail.com>
2021-03-12 00:16:08 +0900

    add PrimeNumber question

--- src/Tests/PrimeNumber.java
+++ src/Tests/PrimeNumber.java
@@ -1,9 +1,29 @@
 package Tests;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class PrimeNumber {
     public static void main(String[] args) {
-        int[] numList = new int[]{};
+        ArrayList<String> answer = new ArrayList<>();
+        int[] numList = new int[100];
+        int count = 0;
 
+        for (int i = 0; i < 100; i++) {
+            numList[i] = i+1;
+        }
+        System.out.println(Arrays.toString(numList));
 
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j <= i; j++) {
+                if(numList[i]%numList[j]==0){
+                    count++;
+                }
+            }
+            if(count==2) answer.add(numList[i]+"");
+            count = 0;
+        }
+        System.out.println(answer);
     }
 }
